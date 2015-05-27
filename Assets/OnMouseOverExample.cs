e98285855d2a177a218cce15ca92bbe9bcc158a0
cneubauern <cneubauern@googlemail.com>
2015-05-27 13:44:57 +0200

    init

---
+++ Assets/OnMouseOverExample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnMouseOverExample : MonoBehaviour 
+{
+	void OnMouseOver()
+	{
+		if(Input.GetMouseButtonDown(0))
+			GetComponent<Renderer>().material.color = Color.red;
+		if(Input.GetMouseButtonDown(1))
+			GetComponent<Renderer>().material.color = Color.blue;
+	}
+}
