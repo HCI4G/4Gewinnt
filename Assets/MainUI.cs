e7f9dc77eae30d698d2bdf2da765710d54b1f59c
cneubauern <cneubauern@googlemail.com>
2015-06-25 12:08:26 +0200

    relocated winMenu

--- Assets/MainUI.cs
+++ Assets/MainUI.cs
@@ -10,8 +10,11 @@ public class MainUI : MonoBehaviour {
 	private float screenWidth;
 	private float buttonHeight;
 	private float buttonWidth;
+	private float menuX;
+
 
 	public static bool startGame  = false;
+	public static bool win; 
 
 	void Start(){
 		if (!startGame){
@@ -21,7 +24,9 @@ public class MainUI : MonoBehaviour {
 		screenHeight = Screen.height;
 		screenWidth = Screen.width;
 		buttonHeight = screenHeight * 0.1f;
-		buttonWidth = screenWidth * 0.3f;
+		buttonWidth = screenWidth * 0.4f;
+		menuX = 0.3f * screenWidth;
+		win = false;
 	}
 
 	string PlayerWho(){
@@ -32,6 +37,28 @@ public class MainUI : MonoBehaviour {
 			plr = "Player 1";
 		return plr;
 	}
+
+	
+	void winMenu () {
+		
+		GUI.Box (new Rect (menuX,buttonHeight,buttonWidth, buttonHeight), "Glückwunsch du hast gewonnen");
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(menuX,2f*buttonHeight,buttonWidth,buttonHeight), "Haupmenü")) {
+			Application.LoadLevel(0);
+		}
+		if(GUI.Button(new Rect(menuX,3.5f*buttonHeight,buttonWidth,buttonHeight), "Neues Spiel")) {
+			MainUI.startGame = true;
+			win = false;
+			
+			WinCheck.getInstance().resetGame();
+		}
+		if(GUI.Button(new Rect(menuX,5f*buttonHeight,buttonWidth,buttonHeight), "Beenden")) {
+			Application.Quit();
+		}
+		if (win)
+			winMenu ();
+	}
+
 
 	void OnGUI(){
 		if (startGame) {
--- Assets/OnMouseOverExample.cs
+++ Assets/OnMouseOverExample.cs
@@ -23,9 +23,6 @@ public class OnMouseOverExample : MonoBehaviour
 
     private WinCheck.SphereState currentActiveUserState;
     private Color color;
-
-
-	private bool win = false;
 	
 	void Start(){
 		
@@ -35,27 +32,7 @@ public class OnMouseOverExample : MonoBehaviour
 		buttonWidth = screenWidth * 0.4f;
 		menuX = screenWidth * 0.3f;
 	}
-	
-    
 
-	void winMenu () {
-
-		GUI.Box (new Rect (menuX,buttonHeight,buttonWidth, buttonHeight), "Glückwunsch du hast gewonnen");
-		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(menuX,2f*buttonHeight,buttonWidth,buttonHeight), "Haupmenü")) {
-			Application.LoadLevel(0);
-		}
-		if(GUI.Button(new Rect(menuX,3.5f*buttonHeight,buttonWidth,buttonHeight), "Neues Spiel")) {
-			MainUI.startGame = true;
-			win = false;
-			
-            WinCheck.getInstance().resetGame();
-		}
-		if(GUI.Button(new Rect(menuX,5f*buttonHeight,buttonWidth,buttonHeight), "Beenden")) {
-			Application.Quit();
-		}
-	}
-    
 	public Boolean doWindow0 = false;
 
 	void DoWindow0(int windowID) {
@@ -89,9 +66,7 @@ public class OnMouseOverExample : MonoBehaviour
 
 		if (doWindow0)
 			GUI.Window(0,new Rect(Screen.height/3,Screen.width/3,Screen.height/3,Screen.width/3),DoWindow0,"Richtige Kugel???");
-
-		if (win)
-			winMenu ();
+	
 
 	}
 	
@@ -180,7 +155,7 @@ public class OnMouseOverExample : MonoBehaviour
             } 
 			MainUI.startGame = false;
 
-			win = true;
+			MainUI.win = true;
 
         }
     }
